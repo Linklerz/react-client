df582fba2cfd5fd07ebeb29a5a65aa97754026e6
sid86-dev <sid86harth@gmail.com>
2022-01-24 03:37:40 +0530

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React, {useEffect, useState} from 'react';
+import React from "react";
+import './App.css';
 
-export default function App() {
-    const [data, setData] = useState([{}])
-    useEffect(() => {
-        fetch("/api/sid86_").then(
-            res => res.json()
-        ).then(data => {
-            setData(data)
-            console.log(data)
-        });
-    }, []);
+class App extends React.Component {
 
-    return (<div>
-            <ul>
-                {data.links.map(item => (
-                    <li key={item.link}>
-                        {item.name}
-                    </li>
-                ))}
-            </ul>
+    // Constructor
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+
+    // ComponentDidMount is used to
+    // execute the code
+    componentDidMount() {
+        fetch(
+"https://linklerz-api.cleverapps.io/api/links/sid86_")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Pleses wait some time.... </h1> </div> ;
+
+        return (
+        <div className = "App">
+            <h1> Fetch data from an api in react </h1> {
+                items.map((item) => (
+                <ol key = { item.id } >
+                    Name: { item.name },
+                    Url : { item.url },
+                    </ol>
+                ))
+            }
         </div>
-
-    )
+    );
 }
+}
+
+export default App;
