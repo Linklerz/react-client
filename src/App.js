e0b364109a5d68c683790b34aca6168749747e2f
sid86-dev <sid86harth@gmail.com>
2022-01-27 02:35:30 +0530

    bugfix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import User from './components/User';
-
 class App extends Component {
     render() {
         return (
             <Router>
                 <Switch>
-                    <Route exact path='/:userName' component={User} />
-
+                    <Route exact path='/li.:userName' component={User} />
+                    <Route>
+                        <div>Default page</div>
+                    </Route>
                 </Switch>
             </Router>
         );
